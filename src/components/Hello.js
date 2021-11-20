cc39705bfd0db043ce6108c51ed04d5cb9482612
JM-Flores <juanmiguel.flores.cics@ust.edu.ph>
2021-11-20 15:14:08 +0800

    Create Hello.js

---
+++ src/components/Hello.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import helloService from "../services/helloService";
+
+const Hello = () =>{
+
+    const [hello, setHello] = useState("test")
+
+    useEffect(() => {
+        helloService.getHello()
+        .then(
+        response => {
+            setHello(response.data);
+        }
+        )
+        .catch(
+            err => {
+                console.log("something went wrong")
+            }
+        )
+    })
+
+    return hello;
+}
+
+export default Hello;
